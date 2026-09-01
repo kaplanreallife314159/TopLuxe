82f99317489e637bff86bebc5d4820bb3e82e414
kaplanreallife314159 <kaplanreallife314159@gmail.com>
2026-09-01 13:49:01 +0100

    Change UserGetPayload to UserPayload in UserRepository

--- apps/web/modules/identity/repositories/UserRepository.ts
+++ apps/web/modules/identity/repositories/UserRepository.ts
@@ -1,7 +1,7 @@
 import type { Prisma, PrismaClient } from '@prisma/client';
 import type { VerifiedPiIdentity } from '@/modules/identity/types/session';
 
-type UserWithRolesRecord = Prisma.UserGetPayload<{
+type UserWithRolesRecord = Prisma.UserPayload<{
   include: { userRoles: { include: { role: true } } };
 }>;
 
